44855d1caa0e17716fc256499acc5cffd5494efb
mahmoudkamal01 <mahmoudkamalmk01@gmail.com>
2024-07-02 12:13:38 +0300

    feat: add header component

---
+++ src/components/common/Header/Header.tsx
@@ -0,0 +1,39 @@
+import { Badge, Container, Nav, Navbar } from "react-bootstrap";
+import styles from "./styles.module.css";
+const { headerContainer, headerLogo } = styles;
+const Header = () => {
+  return (
+    <header>
+      <div className={headerContainer}>
+        <h1 className={headerLogo}>
+          <span>our </span>
+          <Badge bg="info">Ecomm</Badge>
+        </h1>
+        {/*basket*/}
+      </div>
+      <Navbar
+        expand="lg"
+        className="bg-body-tertiary"
+        bg="dark"
+        data-bs-theme="dark"
+      >
+        <Container>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Categories</Nav.Link>
+              <Nav.Link href="#link">About</Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link href="#home">Login</Nav.Link>
+              <Nav.Link href="#link">Register</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
