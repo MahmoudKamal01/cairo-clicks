6172be04f1f98e28b8a78b71e17b95270f6ecaf3
mahmoudkamal01 <mahmoudkamalmk01@gmail.com>
2024-07-03 11:30:13 +0300

    feat: add categories and products pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom/client";
 import AppRouter from "@routes/AppRouter";
-
+import "./styles/global.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(<AppRouter />);
--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -6,15 +6,32 @@ import Products from "@pages/Products";
 import About from "@pages/About";
 import Login from "@pages/Login";
 import Register from "@pages/Register";
+import Error from "@pages/Error";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout />,
+    errorElement: <Error />,
     children: [
       { index: true, element: <Home /> },
       { path: "categories", element: <Categories /> },
-      { path: "products/:prefix", element: <Products /> },
+      {
+        path: "products/:prefix",
+        element: <Products />,
+        loader: ({ params }) => {
+          if (
+            typeof params.prefix !== "string" ||
+            !/^[a-z]+$/i.test(params.prefix)
+          ) {
+            throw new Response("Bad Request", {
+              statusText: "Category not found",
+              status: 400,
+            });
+          }
+          return true;
+        },
+      },
       { path: "about-us", element: <About /> },
       { path: "login", element: <Login /> },
       { path: "register", element: <Register /> },
